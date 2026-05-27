7cbd3dee43a7e600201634c7f0516013035409bc
Damir <dza62@sfu.ca>
2026-05-27 17:14:52 +0300

    ml-fundamentals: tried linear regression for many features with gradient descend, also implementing standardize function to standardize all features.

--- ml-fundamentals/linear-regression/MultipleLinearRegressionClass.py
+++ ml-fundamentals/linear-regression/MultipleLinearRegressionClass.py
@@ -1,5 +1,5 @@
 class MultipleLinearRegression:
-    def __init__(self, n_features=0, lr=0.01):
+    def __init__(self, n_features, lr=0.01):
         self.b = 0.0
         self.weights = [0.0] * n_features
         self.lr = lr
@@ -22,22 +22,23 @@ class MultipleLinearRegression:
         n = len(y)
         dw = []
         db = (2/n) * sum((i - j) for i, j in zip(y_pred, y))
-        for i in len(self.weights):
-            x = X[:, i]
-            grad = (2/n) * sum(xi * (j - k) for xi, j, k in zip(x, y_pred, y))
+        for i in range(len(X[0])):
+            xi = []
+            for x in X:
+                xi.append(x[i])
+            grad = (2/n) * sum((j - k) * xii for xii, j, k in zip(xi, y_pred, y))
             dw.append(grad)
         return dw, db
     
     def fit(self, X, y, epochs=1000, print_epoch=200):
-        for i in epochs:
+        for i in range(epochs):
             dw, db = self.grad_compute(X, y)
-            self.weights -= self.lr * dw
+            self.weights = [weight - self.lr * dwi for weight, dwi in zip(self.weights, dw)]
             self.b -= self.lr * db
             cost = self.cost_compute(X, y)
             self.cost_history.append(cost)
             if i % print_epoch == 0:
                 print(f"Cost {cost:.4f} at epoch {i:4d}")
-            print(self.weights, self.b)
         return self
     
     def r_squared(self, X, y):
--- ml-fundamentals/linear-regression/linear-regression-from-scratch.py
+++ ml-fundamentals/linear-regression/linear-regression-from-scratch.py
@@ -2,12 +2,16 @@ import random
 import math
 from LinearRegressionClass import LinearRegression
 from LinearRegressionNormalClass import LinearRegressionNormal
+from MultipleLinearRegressionClass import MultipleLinearRegression
 
 random.seed(90)
 
 WEIGHT = 3.0
 BIAS = 7.0
 N_SAMPLES = 100
+
+WEIGHT_MULT = [50, 10000, -1000]
+BIAS_MULT = 5000
 
 X = [random.uniform(0, 10) for _ in range(N_SAMPLES)]
 y = [WEIGHT * x + BIAS + random.gauss(0, 2) for x in X] 
@@ -26,3 +30,29 @@ model_normal.fit(X, y)
 
 print(f"Normal Equation Linear Regression {model_normal.w:.4f} and {model_normal.b:.4f}")
 print(f"{model_normal.r_squared(X, y):.4f}")
+
+X_mult = []
+y_mult = []
+for _ in range(N_SAMPLES):
+    size = random.uniform(500, 3000)
+    bedrooms = random.randint(1, 5)
+    age = random.uniform(0, 50)
+    price = WEIGHT_MULT[0] * size + WEIGHT_MULT[1] * bedrooms + WEIGHT_MULT[2] * age + BIAS_MULT + random.gauss(0, 20000)
+    X_mult.append([size, bedrooms, price])
+    y_mult.append(price)
+
+def standardize(X):
+    for i in range(len(X[0])):
+        xi = []
+        for x in X:
+            xi.append(x[i])
+        xi_mean = sum(xi) / len(xi)
+        xi_variance = sum((xii - xi_mean) ** 2 for xii in xi) / len(xi)
+        
+    pass
+
+model_mult = MultipleLinearRegression(n_features=3)
+model_mult.fit(X_mult, y_mult)
+
+print(model_mult.weights)
+print(model_mult.b)
